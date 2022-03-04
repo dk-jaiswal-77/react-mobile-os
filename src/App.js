a573f184f1c3d8c53299e970de81811f868e6e63
dk-jaiswal-77 <dkjaiswal771999@gmail.com>
2022-03-04 22:17:18 +0545

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const data = [
+  {
+    heading : "Mobile Operating System",
+    l1 : "Android",
+    l2 : "Blackberry",
+    l3 : "iPhone",
+    l4 : "Windows Phone"
+  },
+  {
+    heading : "Mobile Manufacturers",
+    l1 : "Samsung",
+    l2 : "HTC",
+    l3 : "Micromax",
+    l4 : "Apple"
+  }
+];
+
+function boxComponent(datum){
+  return (
+    <div>
+      <h1>{datum.heading}</h1>
+      <ul>
+        <li>{datum.l1}</li>
+        <li>{datum.l2}</li>
+        <li>{datum.l3}</li>
+        <li>{datum.l4}</li>
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(boxComponent)}
     </div>
   );
 }
